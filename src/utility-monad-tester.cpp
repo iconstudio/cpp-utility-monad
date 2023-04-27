df54a8e5503b0bb849b8b49dd46b39ded9b8677c
Iconer <yoyofa2@hotmail.com>
2023-04-28 03:07:50 +0900

    A Little Test Cases

---
+++ src/utility-monad-tester.cpp
@@ -0,0 +1,11 @@
+﻿#include "utility-monad-tester.hpp"
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+
+	std::cout << "Hello Monad." << std::endl;
+	util::tests::test_monad();
+
+	return 0;
+}
